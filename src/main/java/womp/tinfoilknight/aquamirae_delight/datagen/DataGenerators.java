cd610b64fb4386a4c92b7819076882355328a29c
tinfoilknight_ <dino.daniel345@gmail.com>
2026-03-13 20:01:34 -0700

    added translations for 简体中文（中国大陆）[Chinese Simplified (China; Mandarin)] and Русский [Russian]

--- src/main/java/womp/tinfoilknight/aquamirae_delight/datagen/DataGenerators.java
+++ src/main/java/womp/tinfoilknight/aquamirae_delight/datagen/DataGenerators.java
@@ -26,6 +26,8 @@ public class DataGenerators {
         gen.addProvider(event.includeClient(), new AQDItemModels(output, existingFileHelper));
         gen.addProvider(event.includeClient(), new AQDBlockStates(output, existingFileHelper));
         gen.addProvider(event.includeClient(), new AQDLang_EN_US(output, "en_us"));
+        gen.addProvider(event.includeClient(), new AQDLang_RU_RU(output, "ru_ru"));
+        gen.addProvider(event.includeClient(), new AQDLang_ZH_CN(output, "zh_cn"));
         gen.addProvider(event.includeServer(), new AQDRecipes(output));
         gen.addProvider(event.includeServer(), blockTags);
         gen.addProvider(event.includeServer(), new AQDItemTags(output, lookupProvider, blockTags.contentsGetter(),existingFileHelper));
